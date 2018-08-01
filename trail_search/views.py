0ea0df71590297d05d71d829b25a13ab8b26d81d
Aaron Emery <aemery14@gmail.com>
2018-08-01 18:07:05 -0400

    pass geo location using js

--- trail_search/views.py
+++ trail_search/views.py
@@ -20,18 +20,9 @@ class SearchView(FormView):
             if self.request.GET['search'] == 'search':
                 endpoint = f'https://trailapi-trailapi.p.mashape.com/?q[activities_activity_type_name_eq]=mountain+biking&q[city_cont]={city}&q[state_cont]={state}'
             else:
-                x_forwarded_for = self.request.META.get('HTTP_X_FORWARDED_FOR')
-                if x_forwarded_for:
-                    ip = x_forwarded_for.split(',')[0]
-                else:
-                    ip = self.request.META.get('REMOTE_ADDR')
-                send_url = f'http://api.ipstack.com/{ip}?access_key=a771658ecb49482642371db0880690ea'
-                r = requests.get(send_url)
-                j = json.loads(r.text)
-                print(j)
-                lat = j['latitude']
-                lon = j['longitude']
-                endpoint = f'https://trailapi-trailapi.p.mashape.com/?q[activities_activity_type_name_eq]=mountain+biking&lat={lat}&lon={lon}'
+                lat = self.request.GET['lat'] if 'lat' in self.request.GET else ''
+                lon = self.request.GET['lon'] if 'lon' in self.request.GET else ''
+                endpoint = f'https://trailapi-trailapi.p.mashape.com/?q[activities_activity_type_name_eq]=mountain+biking&lat={lat}&lon={lon}&radius=25'
             s = requests.Session()
             s.headers.update({'Accept': 'text/plain', 'X-Mashape-Key': '54lXQ1oTXHmshd3CW3lqEEaJaIt4p1lmHYDjsncC76WrSHS0mp'})
             r = s.get(endpoint)
