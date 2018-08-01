c6574117f5875398610efe50fe08fca88025fb56
Aaron Emery <aemery14@gmail.com>
2018-08-01 16:06:14 -0400

    attempting geo location

--- trail_search/views.py
+++ trail_search/views.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import socket
 
 from django.views.generic import TemplateView, FormView
 from django.urls import reverse_lazy
@@ -16,7 +17,21 @@ class SearchView(FormView):
         if 'search' in self.request.GET:
             city = self.request.GET['city'] if 'city' in self.request.GET else ''
             state = self.request.GET['state'] if 'state' in self.request.GET else ''
-            endpoint = f'https://trailapi-trailapi.p.mashape.com/?q[activities_activity_type_name_eq]=mountain+biking&q[city_cont]={city}&q[state_cont]={state}'
+            if self.request.GET['search'] == 'search':
+                endpoint = f'https://trailapi-trailapi.p.mashape.com/?q[activities_activity_type_name_eq]=mountain+biking&q[city_cont]={city}&q[state_cont]={state}'
+            else:
+                x_forwarded_for = self.request.META.get('HTTP_X_FORWARDED_FOR')
+                if x_forwarded_for:
+                    ip = x_forwarded_for.split(',')[0]
+                else:
+                    ip = self.request.META.get('REMOTE_ADDR')
+                send_url = f'http://api.ipstack.com/{ip}?access_key=a771658ecb49482642371db0880690ea'
+                r = requests.get(send_url)
+                j = json.loads(r.text)
+                print(j)
+                lat = j['latitude']
+                lon = j['longitude']
+                endpoint = f'https://trailapi-trailapi.p.mashape.com/?q[activities_activity_type_name_eq]=mountain+biking&lat={lat}&lon={lon}'
             s = requests.Session()
             s.headers.update({'Accept': 'text/plain', 'X-Mashape-Key': '54lXQ1oTXHmshd3CW3lqEEaJaIt4p1lmHYDjsncC76WrSHS0mp'})
             r = s.get(endpoint)
